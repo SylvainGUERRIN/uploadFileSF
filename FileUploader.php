efd6c96423db02d290c1ef62e6a0dffcb8eb72e2
SylvainGUERRIN <37952127+SylvainGUERRIN@users.noreply.github.com>
2019-06-20 10:01:13 +0200

    ajout fonctions lors de l'upload
    ajout de la gestion de plusieurs répertoires cibles pour l'enregistrement des fichiers
    et ajout de fonctionnalités de suppression avec filesystem

--- FileUploader.php
+++ FileUploader.php
@@ -3,37 +3,67 @@
 
 namespace App\Service;
 
+use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 class FileUploader
 {
-    private $targetDirectory;
+    private $targetDirectoryAvatar;
 
-    public function __construct($targetDirectory)
+    private $targetDirectoryImageArticle;
+
+    public function __construct($targetDirectoryAvatar, $targetDirectoryImageArticle)
     {
-        $this->targetDirectory = $targetDirectory;
+        $this->targetDirectoryAvatar = $targetDirectoryAvatar;
+        $this->targetDirectoryImageArticle = $targetDirectoryImageArticle;
     }
 
-    public function upload(UploadedFile $file, $username, $fileId)
+    public function upload(UploadedFile $file, $name, $fileId, $choiceTarget, $oldImage)
     {
-
+        //création du nom de l'image définitive
         $fileName = md5(uniqid()).'.'.$file->guessExtension();
         $parts = explode('.', $fileName);
-        $replace = str_replace($parts[0], $username . $fileId . '.', $parts);
+        $replace = str_replace($parts[0], $name . $fileId . '.', $parts);
         $finalName = implode($replace);
-
-        try {
-            $file->move($this->getTargetDirectory(), $finalName);
-        } catch (FileException $e) {
-            echo "Une erreur est survenue lors du téléchargement de l'image.";
+        //suppression de l'ancienne image si elle existe pour l'article
+        if($oldImage !== null) {
+            $filesystem = new Filesystem();
+            if($choiceTarget === 1) {
+                $url = 'url pour les avatars' . $oldImage;
+            }else {
+                $url = 'url pour les images' . $oldImage;
+            }
+            $verif = $filesystem->exists($url);
+            if ($verif === true) {
+                $filesystem->remove($url);
+            }
         }
-
-        return $finalName;
+        //enregistrement de l'image avec le repertoire correspondant
+        if($choiceTarget === 1) {
+            try {
+                $file->move($this->getTargetDirectoryAvatar(), $finalName);
+            } catch (FileException $e) {
+                echo "Une erreur est survenue lors du téléchargement de l'image.";
+            }
+            return $finalName;
+        }else{
+            try {
+                $file->move($this->getTargetDirectoryImageArticle(), $finalName);
+            } catch (FileException $e) {
+                echo "Une erreur est survenue lors du téléchargement de l'image.";
+            }
+            return $finalName;
+        }
     }
 
-    public function getTargetDirectory()
+    public function getTargetDirectoryAvatar()
     {
-        return $this->targetDirectory;
+        return $this->targetDirectoryAvatar;
+    }
+
+    public function getTargetDirectoryImageArticle()
+    {
+        return $this->targetDirectoryImageArticle;
     }
 }
